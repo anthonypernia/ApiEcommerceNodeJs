b529a8db919a96de2000be3564b5530a5ab6afae
anthony pernia <anthonyperniah@gmail.com>
2022-02-11 01:36:19 +0000

    init 2da entrega

---
+++ public/cart.js
@@ -0,0 +1,10 @@
+let btn_comeback_to_home = document.getElementById("btn_comeback_to_home");
+
+
+
+
+
+btn_comeback_to_home.addEventListener("click", () => {
+    window.location.href = "index.html";
+    }
+);
